c4a3324bf6858f45f473dba3454d5624c763cac9
Fei <feiyaxiao@hotmail.com>
2022-11-21 10:00:59 -0500

    add two for loops

--- secondAssignment/loops.js
+++ secondAssignment/loops.js
@@ -1,1 +1,22 @@
-//
+//2. a for loop that logs 0-15;
+for (i = 0; i < 16; i++) {
+    console.log(i)
+}
+
+
+//3.a for lopp that counts from 15-0;
+for (i = 15; i >= 0; i--) {
+    console.log(i)
+}
+
+//4. Make variable laps = 0; write a for loop that logs
+// (“that’s another lap!”) and adds 1 to the laps variable. AFTER the loop, log laps.
+let laps = 0;
+for (i = 0; i < 2; i++) {
+    console.log("that's another lap!");
+    laps+=1;
+}
+
+
+//git add. commit, push
+
